9429ccb213972835753c83082ad7649b243d92d1
LumenEcho <pestilliva@gmail.com>
2025-02-08 23:50:35 -0600

    Added retrieval of class json script in the javascript

---
+++ script.js
@@ -0,0 +1,14 @@
+let testDiv = document.getElementById("testDiv");
+getClasses();
+
+//Requests the json file
+//All operations with the json data must be called from within this function
+async function getClasses() {
+    const url="/classFile.json";
+    const response = await fetch(url).then((response) => response.json());
+    let classData = response["classes"];
+    console.log(classData);
+
+    //testDiv.textContent = classData[0]["classId"]
+}
+
