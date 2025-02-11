a06cc11b12a79fe9532d82b91cb89feb7a9dc475
LumenEcho <pestilliva@gmail.com>
2025-02-11 16:59:20 -0600

    Made dynamic datalists for degree and class searching and removed default options

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 let testDiv = document.getElementById("testDiv");
+let degreeSelectionBox = document.getElementById("degreesChoice");
+let classSelectionBox = document.getElementById("classChoice");
+let degreesList = document.getElementById("degrees");
+let classesList = document.getElementById("classes");
+
 getClasses();
+getDegrees();
 
 //Requests the json file
 //All operations with the json data must be called from within this function
@@ -8,10 +14,32 @@ async function getClasses() {
     const response = await fetch(url).then((response) => response.json());
     let classData = response["classes"];
     console.log(classData);
-
+    loadSearches(classData, classesList);
     //testDiv.textContent = classData[0]["classId"]
 }
 
-//Remove lab classes from json file
+async function getDegrees() {
+    const url = "/degrees.json";
+    const response = await fetch(url).then((response) => response.json());
+    let degreeData = response["degrees"];
+    console.log(degreeData);
+    loadSearches(degreeData, degreesList);
+}
 
+
+function loadSearches(data, searchBar) {
+    for (i = 0; i < data.length; i++) {
+        let newOption = document.createElement("option");
+        searchBar.append(newOption);
+        if (searchBar == degreesList) {
+            newOption.value = data[i]["degreeName"];
+        }
+        else if (searchBar == classesList) {
+            classNameString = "";
+            classNameString = data[i]["classId"].concat(" ", data[i]["className"]);
+            newOption.value = classNameString;
+        }
+        
+    }
+}
 
